12aab8f8cbb6bff649420f21f58aff7b74e11da7
Nilo Almeida <nilopaivadealmeida@gmail.com>
2022-05-18 19:39:51 -0300

    Adicionando arquivo de rotas

--- server.ts
+++ server.ts
@@ -1,32 +1,10 @@
 import express from 'express';
 import 'dotenv/config'
-import * as controller from './source/controller';
+import { route } from './source/route';
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(route);
 
-
-app.get('/', (req, res) => {
-    res.json({server: "is on"});
-});
-
-app.post('/create', async (req, res) => {
-    try{
-        const result = await controller.createUser(req.body);
-        res.json(result);
-    }catch(e){
-        res.json({message:e});
-    }
-});
-
-app.get('/user/:id', async (req, res) => {
-    try{
-        const id = parseInt(req.params.id);
-        const result = await controller.getUser(id);
-        res.json(result);
-    }catch(e){
-        res.json({message:e});
-    }
-});
 
 app.listen(process.env.PORT, ()=>{console.log('Server is on')})
---
+++ source/route.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import * as controller from './controller';
+
+export const route = express()
+
+route.get('/', (req, res) => {
+    res.json({server: "is on"});
+});
+
+route.post('/create', async (req, res) => {
+    try{
+        const result = await controller.createUser(req.body);
+        res.json(result);
+    }catch(e){
+        res.json({
+            message:e
+        });
+    }
+});
+
+route.get('/user/:id', async (req, res) => {
+    try{
+        const id = parseInt(req.params.id);
+        const result = await controller.getUser(id);
+        res.json(result);
+    }catch(e){
+        res.json({
+            message:e
+        });
+    }
+});
